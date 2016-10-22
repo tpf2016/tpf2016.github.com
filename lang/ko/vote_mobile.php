61a7e501ce279047de94d3213a38d5f1d1cd8404
tpf2016 <toimulpump@gmail.com>
2016-10-22 10:17:38 +0900

    check2

--- lang/ko/vote_mobile.php
+++
@@ -1,7 +0,0 @@
-<?php include("header.php");?>
-
-<iframe
-src="https://docs.google.com/forms/d/1RbaA6a-HP06TVjfL0ksvKeRebvvPrP2faDVlY48BBtQ/viewform?embedded=true"
-width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">
-	로드 중...
-</iframe>
